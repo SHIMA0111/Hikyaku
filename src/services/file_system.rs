3fbacda459d76e37c0b24bb33a11b2d67f68e6b4
しま/SHIMA <seigo.oshima@little-tabby.com>
2024-11-20 10:46:08 +0900

    add functions

--- src/services/file_system.rs
+++ src/services/file_system.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::sync::Arc;
 use reqwest::Client;
 use tokio::sync::Mutex;
@@ -21,17 +21,57 @@ pub enum FileSystemObject {
         client: Vec<Arc<S3Client>>,
         bucket: String,
         key: String,
-        file_size: Mutex<u64>,
+        file_size: Mutex<Option<u64>>,
     },
     GoogleDrive {
         client: Vec<Arc<Client>>,
         file_or_parent_id: String,
         upload_filename: Option<String>,
         mime_type: String,
-        file_size: Mutex<u64>,
+        file_size: Mutex<Option<u64>>,
     },
     Local {
-        path: Box<dyn AsRef<Path>>,
+        path: PathBuf,
+        file_size: Mutex<Option<u64>>
     },
+}
+
+impl FileSystemObject {
+    async fn init_file_size(&self) {
+        match self {
+            FileSystemObject::AmazonS3 {
+                client, 
+                bucket, 
+                key, 
+                file_size, ..} => {
+                
+            },
+            FileSystemObject::GoogleDrive {
+                client, 
+                file_or_parent_id, 
+                file_size, ..} => {},
+            FileSystemObject::Local {
+                path, 
+                file_size, ..} => {
+                let path = path.as_path();
+                let metadata = path.metadata();
+                
+                match metadata {
+                    Ok(metadata) => {
+                        if metadata.is_file() {
+                            *file_size.lock().await = Some(metadata.len())
+                        }
+                        else {
+                            // If the path is directory, it shouldn't have the length. It is nonsense.
+                            *file_size.lock().await = None;
+                        }
+                    },
+                    Err(_) => {
+                        *file_size.lock().await = None;
+                    }
+                }
+            }
+        }
+    }
 }
 
